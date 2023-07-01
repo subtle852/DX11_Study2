411f4cc5c1be11fa15880ad8ac95ccc063b53625
subtle852 <subtle852@gmail.com>
2023-07-02 02:24:58 +0900

    Add Hierarchy struct (parent child) to Transform

--- YamYamEngine/Engine_SOURCE/yaTransform.cpp
+++ YamYamEngine/Engine_SOURCE/yaTransform.cpp
@@ -47,6 +47,11 @@ namespace ya
 		mUp = Vector3::TransformNormal(Vector3::Up, rotation);
 		mFoward = Vector3::TransformNormal(Vector3::Forward, rotation);
 		mRight = Vector3::TransformNormal(Vector3::Right, rotation);
+
+		if (mParent)
+		{
+			mWorld *= mParent->mWorld;
+		}
 	}
 
 	void Transform::Render()
--- YamYamEngine/YamYamEngine/yaPlayScene.cpp
+++ YamYamEngine/YamYamEngine/yaPlayScene.cpp
@@ -21,19 +21,40 @@ namespace ya
 		// STAGE 01 - BG 
 		{
 			GameObject* mBG_01 = new GameObject();
+			mBG_01->SetName(L"BG_01");
 			AddGameObject(eLayerType::Player, mBG_01);// Player로 설정
 			MeshRenderer* mr = mBG_01->AddComponent<MeshRenderer>();
 			mr->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
 			mr->SetMaterial(Resources::Find<Material>(L"SpriteMaterial_STAGE01_BG"));
 			mBG_01->GetComponent<Transform>()->SetPosition(Vector3(0.0f, 0.0f, 0.0f));
 			//player->AddComponent<CameraScript>();
+
+
+			GameObject* mBG_02 = new GameObject();
+			mBG_02->SetName(L"BG_02");
+			AddGameObject(eLayerType::Player, mBG_02);// Player로 설정
+			MeshRenderer* mr2 = mBG_02->AddComponent<MeshRenderer>();
+			mr2->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
+			mr2->SetMaterial(Resources::Find<Material>(L"SpriteMaterial_STAGE01_BG"));
+			mBG_02->GetComponent<Transform>()->SetPosition(Vector3(1.0f, 0.0f, 0.0f));
+			//player->AddComponent<CameraScript>();
+
+			mBG_02->GetComponent<Transform>()->SetParent(mBG_01->GetComponent<Transform>());
+
+			const float pi = 3.141592f;
+			float degree = pi / 2.0f;
+
+			mBG_01->GetComponent<Transform>()->SetPosition(Vector3(-3.0f, 0.0f, 1.0001f));
+			mBG_01->GetComponent<Transform>()->SetRotation(Vector3(0.0f, 0.0f, degree));
 		}
+
 		{
 			GameObject* mUI = new GameObject();
+			mUI->SetName(L"UI");
 			AddGameObject(eLayerType::UI, mUI);// UI로 설정
-			MeshRenderer* mr = mUI->AddComponent<MeshRenderer>();
-			mr->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
-			mr->SetMaterial(Resources::Find<Material>(L"SpriteMaterial_STAGE01_BG"));
+			MeshRenderer* mr3 = mUI->AddComponent<MeshRenderer>();
+			mr3->SetMesh(Resources::Find<Mesh>(L"RectMesh"));
+			mr3->SetMaterial(Resources::Find<Material>(L"SpriteMaterial_STAGE01_BG"));
 			mUI->GetComponent<Transform>()->SetPosition(Vector3(3.0f, 0.0f, 0.0f));
 			//player->AddComponent<CameraScript>();
 		}
@@ -41,6 +62,7 @@ namespace ya
 		//Main Camera
 		{
 			GameObject* camera = new GameObject();
+			camera->SetName(L"MainCamera");
 			AddGameObject(eLayerType::Player, camera);
 			camera->GetComponent<Transform>()->SetPosition(Vector3(0.0f, 0.0f, -10.0f));
 			Camera* cameraComp = camera->AddComponent<Camera>();
@@ -51,6 +73,7 @@ namespace ya
 		//UI Camera
 		{
 			GameObject* camera = new GameObject();
+			camera->SetName(L"UICamera");
 			AddGameObject(eLayerType::Player, camera);
 			camera->GetComponent<Transform>()->SetPosition(Vector3(0.0f, 0.0f, -10.0f));
 			Camera* cameraComp = camera->AddComponent<Camera>();
